019cea72d5aaefb0acf2c4698dd008034cc878b2
Jackie <sharkaccino@fastmail.com>
2025-11-22 21:19:43 -0800

    404: update layout
    Signed-off-by: Jackie <sharkaccino@fastmail.com>

--- src/components/NotFoundText.tsx
+++ src/components/NotFoundText.tsx
@@ -3,15 +3,14 @@ import { type Component } from "solid-js";
 const NotFoundText: Component = () => {
   const stringOpts = [
     `"it ain't here"`,
-    `"idk what you're looking for"`,
+    `"idk what ur looking for"`,
     `"i'll make it later, maybe"`,
-    `"drake, where's the webpage?"`,
-    `"sorry i was hungry and i ate it :("`,
+    `"sorry i ate it :("`,
     `"did you try turning ur browser off and on again"`,
-    `"maybe if you hit refresh 700,000 times it'll work again"`,
     `"aw dangit"`,
     `"oops"`,
-    `*SMASHES KEYBOARD*`
+    `"shoot"`,
+    `"aaaand it's gone"`
   ];
 
   const getRandomString = (): string => {
