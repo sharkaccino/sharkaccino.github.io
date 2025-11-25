a0cb71bde429574574f550ea77a6462fa1307a96
Jackie <sharkaccino@fastmail.com>
2025-11-24 16:59:17 -0800

    blog: support featured images
    Signed-off-by: Jackie <sharkaccino@fastmail.com>

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,13 @@
 import { defineCollection, z } from "astro:content";
 import { glob, file } from "astro/loaders";
 
-// TODO: support for featured images in blog posts
-
 // TODO: gallery content
 
 const blog = defineCollection({
   loader: glob({ pattern: `**/*.{md,mdx}`, base: `./public/blog` }),
   schema: z.object({
     title: z.string(),
+    image: z.string().optional(),
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     tags: z.array(z.string())
